58773b208365125a2da814a785af4e29f6e383fe
Aflojack <100036327+Aflojack@users.noreply.github.com>
2025-12-04 16:37:54 +0100

    Add 2025 day 4 part 1 solution

---
+++ 2025/Day04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Day04;
+
+try
+{
+    //string path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, @"inputs/inputTest1.txt");
+    string path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, @"inputs/inputPuzzle.txt");
+    var input = new List<string>();
+    using StreamReader reader = new(path);
+    string line;
+    while (true)
+    {
+        line = reader.ReadLine();
+        if(line != null && line != "" && line != "\n")
+        {
+            input.Add(line);
+        }
+        else
+        {
+            break;
+        }
+    }
+    Console.WriteLine($"Total line count: {input.Count}");
+    Console.WriteLine($"Total movable paper roll: {Solution.GetTotalMovablePaper(input, 1, 4)}");
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
---
+++ 2025/Day04/Solution.cs
@@ -0,0 +1,60 @@
+using System;
+using Microsoft.VisualBasic;
+
+namespace Day04;
+
+public static class Solution
+{
+    private const char PaperRoll = '@';
+    private const char EmptySpace = '.';
+    private const char MovablePaperRoll = 'x';
+
+    public static int GetTotalMovablePaper(List<string> inputLines, int range, int paperRollLimit)
+    {
+        char[][] mapArray = [.. inputLines.Select(o=>o.ToCharArray())];
+        char[][] resultArray = [.. inputLines.Select(o=>o.ToCharArray())];
+        var totalMovable = 0;
+        for (var i = 0; i < mapArray.Length; i++)
+        {
+            for (var j = 0; j < mapArray[0].Length; j++)
+            {
+                if (mapArray[i][j] == PaperRoll &&
+                    GetRollNumberInRange(mapArray, i, j, range) < paperRollLimit)
+                {
+                    resultArray[i][j] = MovablePaperRoll;
+                    totalMovable++;
+                }
+            }
+        }
+        return totalMovable;
+    }
+
+    private static int GetRollNumberInRange(char[][] array, int local_i, int local_j, int range)
+    {
+        var total = 0;
+        for (var i = local_i - range; i <= local_i + range; i++)
+        {
+            for (var j = local_j - range; j <= local_j + range; j++)
+            {
+                if(i == local_i && j == local_j)
+                {
+                    continue;
+                }
+                if (GetElementByIndex(array, i, j) == PaperRoll)
+                {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+
+    private static char GetElementByIndex(char[][] array, int i, int j)
+    {
+        if (i < 0 || i >= array.Length || j < 0 || j >= array[0].Length)
+        {
+            return EmptySpace;
+        }
+        return array[i][j];
+    }
+}
